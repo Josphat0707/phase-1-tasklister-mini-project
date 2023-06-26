b5cb2749ae48fcca3c17e5d07a337b60e6963c38
josphat <mjirura0707@gmail.com>
2023-06-26 21:28:14 +0300

    finished project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function() {
+
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const taskDescription = document.getElementById("new_task_description").value;
+
+    if (taskDescription !== "") {
+      const newTask = document.createElement("li");
+      newTask.textContent = taskDescription;
+
+      let btn =document.createElement("button")
+      btn.addEventListener(`click`, deleteTask)
+      btn.textContent = `delete`
+
+      taskList.appendChild(newTask );
+
+      newTask.appendChild(btn)
+    
+      document.getElementById("new_task_description").value = "";
+    }
+    
+  });
 });
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
